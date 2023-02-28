268b34ee1f86dff4104ad952e5fc8c0f6e112c8a
Saran Raj <58212046+saran13raj@users.noreply.github.com>
2023-02-28 13:35:20 +0530

    chore: updated comment

--- index.mjs
+++ index.mjs
@@ -7,8 +7,9 @@ for(const s of size) {
     const obj1 = generateEntry(1, s)
     const obj2 = generateEntry(1, s)
 
+    // sample JSON with diff    
     // const obj1 ={"uid":"1161ff60a9db494c80b2ede57ef1b0fd","type":"doc","attrs":{},"children":[{"type":"p","attrs":{},"uid":"a88c1c76e21e41e68088a28428af9477","children":[{"text":"123"}]},{"type":"p","attrs":{},"uid":"c52149f3daf34759a7431512e90f71ed","children":[{"text":"2"}]},{"type":"p","attrs":{},"uid":"ac69394ae7e149a59bf1f258b24f6b3c","children":[{"text":"3"}]},{"type":"p","attrs":{},"uid":"18858837948142f2b9419c9f9edeb5f4","children":[{"text":"4"}]}]}
-    // const obj2 ={"uid":"1161ff60a9db494c80b2ede57ef1b0fd","type":"doc","attrs":{},"children":[{"type":"p","attrs":{},"uid":"a88c1c76e21e41e68088a28428af9477","children":[{"text":"1"}]},{"type":"p","attrs":{},"uid":"c52149f3daf34759a7431512e90f71ed","children":[{"text":"2"}]},{"type":"p","attrs":{},"uid":"ac69394ae7e149a59bf1f258b24f6b3c","children":[{"text":"3"}]},{"type":"p","attrs":{},"uid":"18858837948142f2b9419c9f9edeb5f4","children":[{"text":"4"}]}]}
+    // const obj2 ={"uid":"1161ff60a9db494c80b2ede57ef1b0fd","type":"doc","attrs":{},"children":[{"type":"p","attrs":{},"uid":"a88c1c76e21e41e68088a28428af9477","children":[{"text":"121"}]},{"type":"p","attrs":{},"uid":"c52149f3daf34759a7431512e90f71ed","children":[{"text":"2"}]},{"type":"p","attrs":{},"uid":"ac69394ae7e149a59bf1f258b24f6b3c","children":[{"text":"3"}]},{"type":"p","attrs":{},"uid":"18858837948142f2b9419c9f9edeb5f4","children":[{"text":"4"}]}]}
     
 
 
@@ -26,7 +27,7 @@ for(const s of size) {
     console.log(`${s/1000}kb | stringify = ${isDirty} ${endTime - startTime}, fast-equal = ${isDirty2} ${endTime2 - startTime2}, lodash = ${isDirty3} ${endTime3 - startTime3} ms`)
 }
 
-// Test results on M1 macbook 13" pro | node v16
+// Test results on M1 13" macbook pro | node v16
 //  300kb stringify= 2.300374984741211 , fast-equal = 0.053458213806152344, lodash =  0.2507500648498535 ms
 //  500kb stringify = 3.4363341331481934 , fast-equal = 0.01574993133544922, lodash =  0.13929224014282227 ms
 //  1024kb stringify = 6.88004207611084 , fast-equal = 0.028374671936035156, lodash =  0.04387474060058594 ms
